30341ce5ccd815bfb0d5d69e1b10102d3d6e98e7
Minhaz Kazi <vlookup@google.com>
2026-06-04 11:52:43 -0700

    Fix OSPO compliance issues (#128)

--- src/__tests__/test_data/mockActiveGit.ts
+++ src/__tests__/test_data/mockActiveGit.ts
@@ -1,1 +1,6 @@
+// © 2019 Google LLC.  All rights reserved.
+//
+// This software is subject to the Google Cloud Terms of Service, as
+// modified by the "General Software Terms" of the Google Cloud Service Specific Terms, available at: https://cloud.google.com/terms/service-terms.
+
 export const mockActiveGit: any = {"name":"master","remote":"origin","remote_name":"master","error":null,"message":null,"owner_name":null,"readonly":false,"personal":false,"is_local":true,"is_remote":true,"is_production":true,"ahead_count":0,"behind_count":0,"commit_at":1613020223,"ref":"01908480e9f8cbafaa63489b69f533671c44b4a6","remote_ref":"01908480e9f8cbafaa63489b69f533671c44b4a6","can":{}}
--- src/components/DiagramFrame/FramePanels/HelpPanelData/index.ts
+++ src/components/DiagramFrame/FramePanels/HelpPanelData/index.ts
@@ -1,3 +1,8 @@
+// © 2019 Google LLC.  All rights reserved.
+//
+// This software is subject to the Google Cloud Terms of Service, as
+// modified by the "General Software Terms" of the Google Cloud Service Specific Terms, available at: https://cloud.google.com/terms/service-terms.
+
 export { view_dimensions } from "./order_items_dimensions"
 export { view_explore } from "./order_items_explore"
 export { join_dimensions } from "./polling_dimensions"
